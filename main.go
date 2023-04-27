a8acdcaffaf6971d98a08cb68aab7e1e7552a30d
Gabriel <contatoeugm@gmail.com>
2023-04-27 14:21:52 -0300

    feat: add custem logger

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/Gabrielm3/Go-Devs-Jobs/router"
+import (
+	"github.com/Gabrielm3/Go-Devs-Jobs/config"
+	"github.com/Gabrielm3/Go-Devs-Jobs/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
 
 	//initialize router
 	router.Initialize()
